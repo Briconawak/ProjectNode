77b5bd339ed1189ab8ea4f763f70b146b2936407
Mathéo DUGUE <54746585+Briconawak@users.noreply.github.com>
2024-12-29 23:44:00 +0100

    Update route_commande.ts

--- Backend/Backend/src/routes/route_commande.ts
+++ Backend/Backend/src/routes/route_commande.ts
@@ -4,45 +4,7 @@ import Produit from '../models/produit';
 
 const orderRouter = Router();
 
-/**
- * @swagger
- * /api/orders:
- *   get:
- *     tags:
- *       - Orders
- *     summary: Get all orders
- *     description: Retrieve all orders with associated product details.
- *     responses:
- *       200:
- *         description: Orders retrieved successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   ID_Commande:
- *                     type: integer
- *                   ID_Produit:
- *                     type: integer
- *                   Nombre:
- *                     type: integer
- *                   Date:
- *                     type: string
- *                     format: date-time
- *                   Produit:
- *                     type: object
- *                     properties:
- *                       Nom:
- *                         type: string
- *                       Prix:
- *                         type: number
- *                       Description:
- *                         type: string
- *       500:
- *         description: Internal server error.
- */
+
 orderRouter.get('/', async (req: Request, res: Response) => {
   try {
     const commandes = await Commande.findAll({
@@ -60,33 +22,7 @@ orderRouter.get('/', async (req: Request, res: Response) => {
   }
 });
 
-/**
- * @swagger
- * /api/orders:
- *   post:
- *     description: Valide la commande en enregistrant les produits dans la base de données
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: array
- *             items:
- *               type: object
- *               properties:
- *                 ID_Produit:
- *                   type: integer
- *                 Nombre:
- *                   type: integer
- *                 Date:
- *                   type: string
- *                   format: date
- *     responses:
- *       201:
- *         description: Commande validée
- *       400:
- *         description: Erreur lors de la validation de la commande
- */
+
 orderRouter.post('/', async (req: Request, res: Response) => {
   const commandesData = req.body;  // On suppose que le corps de la requête contient un tableau d'objets
 
